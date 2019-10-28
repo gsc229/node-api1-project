226e2b9e3d5d00c1ad7e9c2c793e337fc6c6f9d4
Greg <gsc229@gmail.com>
2019-10-28 15:14:08 -0400

    .get(/api/users/)  db.find() working

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // implement your API here
+const express = require('express'); //equivalent to import sthg from sthg
+
+const server = express(); // creates a server 
+const db = require('./data/db.js');  // call in the db functions
+
+//route/request handlers
+server.get('/', (req, res) => {
+
+  res.send('Hello Node 23!');
+
+})
+
+server.get('/api/users/', (req, res) => {
+  db
+    .find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.send(err);
+    })
+})
+
+
+
+
+/* POST | /api/users
+GET | /api/users
+GET | /api/users /: id
+DELETE | /api/users /: id
+PUT | /api/users /: id 
+
+  `find()`
+  `findById()`
+  `insert()`
+  `update()`
+  `remove()` */
+
+
+//listen for requests in a particular port on localhost
+const port = 8000; //localhost 8000
+server.listen(port, () => console.log('\n=== API on port 8000 ===\n'));
